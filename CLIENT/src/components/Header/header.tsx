7f1e9d7b8999074bb9f6860f496971327991c44a
Angel Ortega <ang3l.ortega21@gmail.com>
2023-10-27 10:12:41 -0600

    ADD: FrontEnd Site

---
+++ CLIENT/src/components/Header/header.tsx
@@ -0,0 +1,13 @@
+import React from './react';
+function Header() {
+
+    return (
+    <>
+            <div className="bg-black">
+                Contenido del elemento...
+            </div>
+        </>
+    )
+}
+
+export default Header
