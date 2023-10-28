634d4a1e653cd4afb2dd701d92bd135fc8348c22
jecktor <jecktor@proton.me>
2023-10-28 09:16:45 -0600

    feat: added wouter as app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route } from 'wouter';
+
 import Index from './components/Index'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
+      <Route path="/">
         <Index />
+      </Route>
     </>
   )
 }
-
-export default App
