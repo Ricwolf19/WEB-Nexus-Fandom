20ddedb6666c86a77eb482c9cbcb0958b3ccebb9
Ricwolf19 <rhtc19@gmail.com>
2023-10-29 15:11:18 -0600

    ADD: Functional Maps API 1 Example

---
+++ src/components/Store.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type StoreProps = {
+  name: string;
+  mapSrc: string;
+};
+
+const Store: React.FC<StoreProps> = ({ name, mapSrc }) => {
+  return (
+    <div className="store">
+      <h3>{name}</h3>
+      <div className="map-container">
+        <iframe
+          title={name}
+          width="600"
+          height="450"
+          loading="lazy"
+          src={mapSrc}
+          style={{ border: 0 }}
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Store;
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
-import bg_index from '../../assets/img-index/bg-index.jpg';
+import React from 'react';
+import Store from '../components/Store';
 
-function Index() {
+const Index: React.FC = () => {
+  const mapSrc =
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14001.595690599088!2d-106.12377261284176!3d28.677711100000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea43acff266547%3A0xa5affbbd366009a2!2sMAS%20VIDEOJUEGOS!5e0!3m2!1sen!2smx!4v1698612115158!5m2!1sen!2smx';
 
-    return (
-    <>
-        <div className="w-screen h-screen bg-black">
-            <img src={bg_index} alt="Fondo Dedsec" />
-            <div className='flex content-center w-screen '>
-                    <h2 className='text-white font-bold'>Start...</h2>
-                <div>
-                    <button className='text-white'>Log In</button>
-                    <button className='text-white'>Register</button>
-                </div>
-            </div>
-        </div>
-    </>
-    )
-}
-
-export default Index
+  return (
+    <div className="index-container">
+      <h1>Mapa de Google Maps</h1>
+      <Store name="MAS VIDEOJUEGOS" mapSrc={mapSrc} />
+    </div>
+  );
+};
+export default Index;
