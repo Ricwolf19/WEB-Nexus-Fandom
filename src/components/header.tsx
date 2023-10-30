f98ea186116c786416a168debb05982e7644aece
yorsh130204 <jorge130204@gmail.com>
2023-10-30 13:00:18 -0600

    add header
    adding routes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,11 @@ function Header() {
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold">Logo</div>
                 <nav className="space-x-4">
-                    <a href="/" className="hover:text-gray-300">Home</a>
-                    <a href="" className="hover:text-gray-300">Marvel</a>
-                    <a href="" className="hover:text-gray-300">VideoGames</a>
-                    <a href="" className="hover:text-gray-300">Stores</a>
-                    <a href="/ContactUs" className="hover:text-gray-300">Contact Us</a>
+                    <a href="../pages/index.tsx" className="hover:text-gray-300">Home</a>
+                    <a href="../pages/marvel-site.tsx" className="hover:text-gray-300">Marvel</a>
+                    <a href="../pages/games-side.tsx" className="hover:text-gray-300">VideoGames</a>
+                    <a href="../pages/stores-side.tsx" className="hover:text-gray-300">Stores</a>
+                    <a href="../pages/contact-side.tsx" className="hover:text-gray-300">Contact Us</a>
                 </nav>
             </div>
         </header>
