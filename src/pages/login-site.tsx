bbad1410bf0bd69be2a12daab3ba16e5c859b0b4
Angel Ortega <ang3l.ortega21@gmail.com>
2023-10-30 15:22:54 -0600

    ADD:  Basic LogIn structure
    The basic logIn structure is added.

--- src/pages/login-site.tsx
+++ src/pages/login-site.tsx
@@ -1,8 +1,17 @@
-
+import imgHitman from '../assets/img-login/img-hitman-login.jpg' 
 
 const LogIn = () => {
     return (
-        <div></div>
+        <div className="bg-black h-screen w-screen flex items-center">
+                <img src={imgHitman} alt="" className='h-screen' />
+            <div className='w-full h-full'>
+                <h2 className='text-white'>Log In</h2>
+                <form action="">
+
+                </form>
+                <button className='w-1/2 h-10 block m-auto bg-red-500 text-white font-bold rounded-sm'>Log In</button>
+            </div>
+        </div>
     );
 };
 
