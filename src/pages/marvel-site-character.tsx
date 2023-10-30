5869bf93b036670d07e8f9d876dbbf29073df2af
jecktor <jecktor@proton.me>
2023-10-30 15:30:28 -0600

    feat: added base marvel site character page

--- src/pages/marvel-site-character.tsx
+++ src/pages/marvel-site-character.tsx
@@ -1,5 +1,26 @@
+import { Link, useRoute } from 'wouter';
+import { useFetchData } from '../hooks/useFetchData';
+import { getCharacter } from '../utils/marvelApi';
+
 export default function MarvelSiteCharacter() {
+  const [match, params] = useRoute('/marvel-side/character/:id');
+  const { data: character, loading, error } = useFetchData(getCharacter, params?.id);
+
   return (
-    <div>MarvelSiteCharacter</div>
+    <div>
+      <Link href='/marvel-side'>Back</Link>
+
+      {!match ? <div>Not found</div> :
+
+        loading ? <div>Loading...</div> :
+
+          error ? <div>{error}</div> :
+
+            <div>
+              <h1>{character?.name}</h1>
+            </div>
+      }
+
+    </div>
   );
 }
