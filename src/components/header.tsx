5711dd086d81b2db57de2106c4370aceac9b283c
yorsh130204 <jorge130204@gmail.com>
2023-10-30 20:05:45 -0600

    add header routes
    adding headre routes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,18 @@
+import { Link } from "wouter";
+
 const Header = () => {
     return (
         <header className="bg-black text-white py-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold">Logo</div>
                 <nav className="space-x-4">
-                    <a href="/" className="hover:text-gray-300">Home</a>
-                    <a href="/marvel-side" className="hover:text-gray-300">Marvel</a>
-                    <a href="/games-site" className="hover:text-gray-300">VideoGames</a>
-                    <a href="/stores-side" className="hover:text-gray-300">Stores</a>
-                    <a href="/contact-side" className="hover:text-gray-300">Contact Us</a>
-                    <a href="/register" className='hover:text-gray-300'>Register</a>
-                    <a href="/login" className="hover:text-gray-300">Log In</a>
+                    <Link href="/" className="hover:text-gray-300">Home</Link>
+                    <Link href="/marvel-side" className="hover:text-gray-300">Marvel</Link>
+                    <Link href="/games-site" className="hover:text-gray-300">VideoGames</Link>
+                    <Link href="/stores-side" className="hover:text-gray-300">Stores</Link>
+                    <Link href="/contact-side" className="hover:text-gray-300">Contact Us</Link>
+                    <Link href="/register" className='hover:text-gray-300'>Register</Link>
+                    <Link href="/login" className="hover:text-gray-300">Log In</Link>
                 </nav>
             </div>
         </header>
