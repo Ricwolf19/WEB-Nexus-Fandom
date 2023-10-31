a7035096ae9c9e9dded1fab2bd1392128a70473c
yorsh130204 <jorge130204@gmail.com>
2023-10-30 20:10:08 -0600

    xd
    xd

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -26,7 +26,7 @@ export default function SearchBar({ onSubmit }: Props) {
           type="text"
           value={search}
           onChange={handleSearchChange}
-          placeholder="Search character..."
+          placeholder="Search..."
         />
         <button
           className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
