8efbbdd33bdb93392f68614c611d9da7c7228261
Angel Ortega <ang3l.ortega21@gmail.com>
2023-11-03 13:48:53 -0600

    Change: Header changes
    Changes are made to the Header, because certain elements covered it and did not leave it visible.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link, useLocation } from "wouter";
 import { AiOutlineMenu } from "react-icons/ai";
 import imgLogo from "../assets/logo.jpg";
+// import firebase from "firebase/app"; // Asegúrate de importar Firebase
+import "firebase/auth";
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -11,32 +13,45 @@ const Header = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
+
+  // const handleLogout = () => {
+  //   firebase.auth().signOut() // Cerrar sesión usando Firebase
+  //     .then(() => {
+  //       // Cierre de sesión exitoso, puedes redirigir al usuario a la página de inicio de sesión o hacer lo que sea necesario.
+  //     })
+  //     .catch((error) => {
+  //       // Maneja los errores si ocurrieron al cerrar la sesión.
+  //       console.error(error);
+  //     });
+  // };
+
+
   return (
-    <nav className="bg-gray-950 text-white p-4 m:py-4 sticky top-0 z-10">
+    <nav className="bg-gray-950 text-white p-4 m:py-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex justify-center items-center">
           <img className="w-16" src={imgLogo} alt="" />
           <h1 className="font-bold text-2xl md:text-3xl">Nexus-Fandom</h1>
         </div>
         <div className="hidden md:flex space-x-4">
-          <Link href="/home" className={`hover:text-gray-300 ${location === "/home" ? "border-b-2 border-yellow-300" : ""}`}>
+          <Link href="/home" className={`hover:text-gray-300 ${location === "/home" ? "border-b-2 border-yellow-300" : "hover:border-b-2 hover:border-yellow-300"}`}>
             Home
           </Link>
-          <Link href="/marvel-side" className={`hover:text-gray-300 ${location === "/marvel-side" ? "border-b-2 border-red-500" : ""}`}>
+          <Link href="/marvel-side" className={`hover:text-gray-300 ${location === "/marvel-side" ? "border-b-2 border-red-500" : "hover:border-b-2 hover:border-red-500"}`}>
             Marvel
           </Link>
-          <Link href="/games-site" className={`hover:text-gray-300 ${location === "/games-site" ? "border-b-2 border-sky-500" : ""}`}>
+          <Link href="/games-site" className={`hover:text-gray-300 ${location === "/games-site" ? "border-b-2 border-sky-500" : "hover:border-b-2 hover:border-sky-500"}`}>
             VideoGames
           </Link>
-          <Link href="/stores-side" className={`hover:text-gray-300 ${location === "/stores-side" ? "border-b-2 border-green-500" : ""}`}>
+          <Link href="/stores-side" className={`hover:text-gray-300 ${location === "/stores-side" ? "border-b-2 border-green-500" : "hover:border-b-2 hover:border-green-500"}`}>
             Stores
           </Link>
-          <Link href="/contact-side" className={`hover:text-gray-300 ${location === "/contact-side" ? "border-b-2 border-indigo-500" : ""}`}>
+          <Link href="/contact-side" className={`hover:text-gray-300 ${location === "/contact-side" ? "border-b-2 border-indigo-500" : "hover:border-b-2 hover:border-indigo-500"}`}>
             Contact Us
           </Link>
         </div>
         <div className="hidden md:flex gap-6">
-          <button className="border-2 border-red-500 bg-red-500/30 hover:bg-red-500 p-2 rounded font-bold">
+          <button  className="border-2 border-red-500 bg-red-500/30 hover:bg-red-500 p-2 rounded font-bold">
             Log Out
           </button>
         </div>
@@ -50,21 +65,24 @@ const Header = () => {
       {mobileMenuOpen && (
         <div className="w-full h-screen p-5 md:hidden absolute bg-gray-900 ">
           <div className="flex flex-col space-y-4">
-            <Link href="/home" className={`hover:text-gray-300 ${location === "/home" ? "border-b-2 border-yellow-500" : ""}`}>
+            <Link href="/home" className={`hover:text-gray-300 ${location === "/home" ? "border-b-2 border-yellow-500" : "hover:border-b-2 hover:border-yellow-500"}`}>
               Home
             </Link>
-            <Link href="/marvel-side" className={`hover:text-gray-300 ${location === "/marvel-side" ? "border-b-2 border-red-500" : ""}`}>
+            <Link href="/marvel-side" className={`hover:text-gray-300 hover:border-b-2 hover:border-red-500 ${location === "/marvel-side" ? "" : "hover:border-b-2 hover:border-red-500"}`}>
               Marvel
             </Link>
-            <Link href="/games-site" className={`hover:text-gray-300 ${location === "/games-site" ? "border-b-2 border-sky-500" : ""}`}>
+            <Link href="/games-site" className={`hover:text-gray-300 ${location === "/games-site" ? "border-b-2 border-sky-500" : "hover:border-b-2 hover:border-sky-500"}`}>
               VideoGames
             </Link>
-            <Link href="/stores-side" className={`hover:text-gray-300 ${location === "/stores-side" ? "border-b-2 border-green-500" : ""}`}>
+            <Link href="/stores-side" className={`hover:text-gray-300 ${location === "/stores-side" ? "border-b-2 border-green-500" : "hover:border-b-2 hover:border-green-500"}`}>
               Stores
             </Link>
-            <Link href="/contact-side" className={`hover:text-gray-300 ${location === "/contact-side" ? "border-b-2 border-indigo-500" : ""}`}>
+            <Link href="/contact-side" className={`hover:text-gray-300 ${location === "/contact-side" ? "border-b-2 border-indigo-500" : "hover:border-b-2 hover:border-indigo-500"}`}>
               Contact Us
             </Link>
+            <button  className={`hover:text-gray-300 hover:border-b-2 hover:border-red-500`}>
+              Log Out
+            </button>
           </div>
         </div>
       )}
